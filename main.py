e4014630c9fb71afedb5c05030432090478dbcf3
lukeosullivan <lbdos32@gmail.com>
2025-11-17 17:44:42 +0000

    Added nav bar to all pages and temporary image to committee.html

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
-@app.route("/static-example/img")
-def static_example_img():
-    start = '<img src ="'
-    url = url_for('static', filename ='screenshot_test.png')
-    end = '">'
-    return start + url + end , 200
+@app.route("/healthAndSafety")
+def health_and_safety():
+    return render_template('healthAndSafety.html')
+
+@app.route("/committee")
+def committee():
+    return render_template('committee.html')
