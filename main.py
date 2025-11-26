2e38e96e57d0a6f57c3e672e8ddafababb6bfba3
lukeosullivan <lbdos32@gmail.com>
2025-11-26 14:24:09 +0000

    Added some CSS to the embedded instagram post and let the server cache the image for up to 5 minutes at a time.

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from flask import Flask, render_template, url_for, jsonify
+from flask_caching import Cache
 import requests
 
 app = Flask(__name__)
+
+app.config['CACHE_TYPE'] = 'SimpleCache'  # for production, use Redis or Memcached
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300  # cache duration in seconds (5 minutes)
+
+cache = Cache(app)
+BEHOLD_JSON_URL = 'https://feeds.behold.so/NMQlaZ9tJIAuwjIG7RIv'
 
 @app.route("/")
 def root():
     return render_template('index.html')
 
-BEHOLD_JSON_URL = 'https://feeds.behold.so/NMQlaZ9tJIAuwjIG7RIv'
-
 @app.route('/latest-instagram')
+@cache.cached()  # this caches the route automatically
 def latest_instagram():
     try:
         res = requests.get(BEHOLD_JSON_URL)
         res.raise_for_status()
         data = res.json()
-        # return only the latest post
-        return jsonify(data['posts'][0])
+        latest_post = data['posts'][0]
+        return jsonify(latest_post)
     except Exception as e:
         print("Error fetching Behold JSON:", e)
         return jsonify({'error': 'Failed to fetch Instagram post'}), 500
-
 
 @app.route("/healthAndSafety")
 def health_and_safety():
